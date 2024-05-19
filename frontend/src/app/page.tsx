dc722c3787003c8d68aed7b24dba26186e2e9598
Ilona Poltavets <20ilona.poltavets02@gmail.com>
2024-05-19 18:57:09 +0300

    added new files

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,25 @@
+const API_URL = process.env.API_URL;
+const API_TOKEN = process.env.API_TOKEN;
+
+export default async function Home() {
+  // console.log(`url: ${API_URL}`);
+  // const result = await fetch(`${API_URL}/api/tests`, {
+  //   headers: {
+  //     Authorization: `Bearer  ${API_TOKEN}`,
+  //     "Content-type": "multipart/form-data",
+  //   },
+  // });
+
+  const result = await fetch(`${API_URL}/api/tests`, {
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+      "Content-type": "multipart/form-data"
+    },
+  });
+
+  const { data } = await result.json();
+  console.log(data);
+  return (
+    <div>Test</div>
+  );
+}
